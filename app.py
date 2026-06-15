81bb2fa0bca99cb17d77a7f738b06bf762f81cde
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-15 15:39:06 +0000

    bug preventing the launch of live mode , inspecting

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from source import Button,callback,last_s
 import sounddevice as sd
+import numpy as np
 pygame.init()
 font=pygame.font.Font(None,30)
 screen=pygame.display.set_mode((1280,720))
@@ -11,9 +12,9 @@ impo=Button(450,350,150,75,"import a file",pygame.Color('green'),pygame.Color('d
 live =Button(650,350,150,75,"live mode",pygame.Color('blue'),pygame.Color('dark blue'),pygame.Color('black'))
 weltxt="welcome to audio visualizer remake ! choose your input :"
 txt1=font.render(weltxt,True,pygame.Color('white'))
+stream=sd.InputStream(callback=callback,channels=2,samplerate=44100,blocksize=2024)
 
 while True:
-    stream=sd.InputStream(callback=callback,channels=2,samplerate=44100,blocksize=2024)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
@@ -31,7 +32,13 @@ while True:
         live.draw(screen)
         screen.blit(txt1,(385,100))
     elif mode==1:
-        for b in last_s:
-            s
+        #brother they get it so easy with the libraries and shi , like , everything is just a fonction u dont have to do anything 
+        stream=np.array_split(last_s,120)
+        bar_val=[np.mean(bar) for bar in stream]
+        xb=40
+        for b in bar_val:
+            pygame.draw.rect(screen,pygame.Color('white'),(xb,720-b,b,10))
+            xb+=10
+            
             
     pygame.display.flip()
